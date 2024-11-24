d1c84e65e616f0deaa244249b900eda67890547f
Manav Chaudhary <120240843+manav4499@users.noreply.github.com>
2024-11-24 16:40:55 -0500

    Also included the structure for the Dashboard and FishCatchMap
    Hi Guys !
    
    Implemented the structure and now we can distribute to start working
    
    Co-Authored-By: Juan <69006778+marvel987dc@users.noreply.github.com>
    Co-Authored-By: Rishi-Goyal-com <177606801+Rishi-Goyal-com@users.noreply.github.com>
    Co-Authored-By: Jainam2301 <149920873+Jainam2301@users.noreply.github.com>

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import Login from './auth/login';
 import Signup from './auth/Signup';
+import Dashboard from './dashboard/dashboard';
+import FishCatchMap from './FishCatchMap';
 import './App.css';
 
 function App() {
   return (
     <>
 
-    {/* Have to work on the login for this  */}
-       <Login />;
+    {/* Have to work on the logic on how to print all components  */}
+       <Login />
        <Signup />
+       <Dashboard/>
+       <FishCatchMap/>
     </>
   )
 }
