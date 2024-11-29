ceadef6fe0ae468edf6b2b3ba1f7950a538967e0
ReactRepo <barrerojuan810@gmail.com>
2024-11-29 14:00:24 -0500

    login  Update on server

--- Backend/server.js
+++ Backend/server.js
@@ -68,6 +68,22 @@ app.post('/signup', async (req, res) => {
   }
 });
 
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+    res.status(200).json({ message: 'Login successful', username });
+  }catch {
+    res.status(500).json({ error: 'Error logging in' });
+  }
+});
 
 
 
